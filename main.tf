84ef42ffa1838a63084f991b95b6546e1e20bf83
noevazz <jonovagu@gmail.com>
2024-08-02 20:28:32 -0600

    fixed name in variable

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 module "my_vcn01" {
     source = "./vcn_module"
     compartment_id = var.compartment_ocid
-    cidr_block = var.vcn02_cidr_block
-    dns_label = var.vcn02_display_name
-    display_name = var.vcn02_display_name
+    cidr_block = var.vcn01_cidr_block
+    dns_label = var.vcn01_display_name
+    display_name = var.vcn01_display_name
 }
 
 module "my_vcn02" {
--- variables.tf
+++ variables.tf
@@ -2,5 +2,5 @@ variable "region" {}
 variable "compartment_ocid" {
     description = "sandobx conpartment"
 }
-variable "vcn02_cidr_block" {}
-variable "vcn02_display_name" {}
+variable "vcn01_cidr_block" {}
+variable "vcn01_display_name" {}
